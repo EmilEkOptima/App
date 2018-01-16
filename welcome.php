79720457c7e96ca36d271b3afd698866f7c6c388
Emil Ek <Emil.ek@optistud.fi>
2018-01-16 12:29:55 +0200

    I added a logout message and sql something

--- welcome.php
+++ welcome.php
@@ -5,8 +5,11 @@
  ?>
  <?php include 'includes/head.php'; ?>
   <body>
-
+    <?php if($_SESSION['username']) : ?>
     <h1>Roses are red, Violets are blue, This name is great (<?php echo $_SESSION['username']; ?>), Just like the user too.</h1>
-
+    <a href="logut.php">Logga ut <?php echo $SEASSION['username']; ?></a>
+  <?php else : ?>
+  <h1>You better get the fuck out, Your not allowed, Except for your mom, owwwwwwwwwwwwwwww seriously get the fuck out</h1>
+<?php endif ; ?>
   </body>
 </html>
